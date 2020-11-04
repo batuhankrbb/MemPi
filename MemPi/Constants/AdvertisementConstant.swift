88aa7ecd355103dc924a0a7412dd4c24fadebef1
Batuhan Karababa <59976112+batuhankrbb@users.noreply.github.com>
2020-11-04 08:23:15 +0300

    Update AdvertisementConstant.swift

--- MemPi/Constants/AdvertisementConstant.swift
+++ MemPi/Constants/AdvertisementConstant.swift
@@ -10,22 +10,22 @@ import Foundation
 enum ct_Ads{
 
     //Banner Ads
-    static let banner_allDigits = "ca-app-pub-3108242646730560/9357850814"
+    static let banner_allDigits = ""
 
-    static let banner_trainingStart = "ca-app-pub-3108242646730560/1616582851"
+    static let banner_trainingStart = ""
 
     //Rewarded Ads
-    static let rewarded_guessStart = "ca-app-pub-3108242646730560/6629236380"
+    static let rewarded_guessStart = ""
 
-    static let rewarded_guessChallangeNotEnough = "ca-app-pub-3108242646730560/9467152711"
+    static let rewarded_guessChallangeNotEnough = ""
 
-    static let rewarded_gameOver = "ca-app-pub-3108242646730560/1204367939"
+    static let rewarded_gameOver = ""
 
-    static let rewarded_trainingStart = "ca-app-pub-3108242646730560/8640499665"
+    static let rewarded_trainingStart = ""
 
     //Interstitial Ads
-    static let interstitial_guessChallange = "ca-app-pub-3108242646730560/4596818033"
+    static let interstitial_guessChallange = ""
 
-    static let interstitial_training = "ca-app-pub-3108242646730560/1971806075"
+    static let interstitial_training = ""
     
 }

472c8d7bb1941c25b2ab32cca51721cd793149e0
Batuhan Karababa <59976112+batuhankrbb@users.noreply.github.com>
2021-08-31 10:27:08 +0300

    fix: names

--- MemPi/Constants/ImageConstant.swift
+++ MemPi/Constants/ImageConstant.swift
@@ -14,10 +14,10 @@ enum ct_Image{
     static let recordIcon = UIImage(named: "recordIcon")!
     static let lastTimeIcon = UIImage(named: "lastTimeIcon")!
     static let memorizeIcon = UIImage(named: "memorizeIcon")!
-    static let appLogo = UIImage(named: "appLogo")!
+    static let appLogo = UIImage(named: "AppLogo")!
     static let hearts3 = UIImage(named: "hearts3")!
-    static let hearts2 = UIImage(named: "hearts2")!
-    static let hearts1 = UIImage(named: "hearts1")!
+    static let hearts2 = UIImage(named: "Hearts2")!
+    static let hearts1 = UIImage(named: "Hearts1")!
     static let hearts0 = UIImage(named: "hearts0")!
     static let hintIcon = UIImage(named: "hintIcon")!
     static let answerIcon = UIImage(named: "answerIcon")!
